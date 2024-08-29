00af7c2211e0720ead7119f8c2466a269cefe0d9
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-08-29 23:03:47 +0300

    Learnt time NB fav lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,22 @@
 # your code goes here!
+import time
+
+def countdown(start):
+    while start > 0:
+        print(f"{start} SECOND(S)!")
+        start -=1
+    print("HAPPY NEW YEAR!")
+    pass
+
+def countdown_with_sleep(start):
+    while start > 0:
+        # if start == 1:
+        #     print(f'{start} SECOND!')
+        # else:
+        print(f"{start} SECOND(S)!")
+        time.sleep(1)
+        start -=1
+    print("HAPPY NEW YEAR!")
+    pass
+
+countdown_with_sleep(15)
